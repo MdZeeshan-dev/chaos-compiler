3de331f5b2a1db09dd5f6ad42e50b13dbaed8417
Md Zeeshan <zeeshan.codev@gmail.com>
2026-06-23 14:17:31 +0530

    h

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -6,22 +6,20 @@
 
 // export default login
 
-
 import { useNavigate } from "react-router-dom";
 const Login = () => {
+  const navigate = useNavigate();
 
-    const navigate = useNavigate();
-
-    function handlelogin() {
-        navigate("/");
-    }
+  function handlelogin() {
+    navigate("/");
+  }
   return (
     <div>
-        <h1>Login page</h1>
+      <h1>Login page</h1>
 
-        <button onClick={handlelogin}>login</button>
+      <button onClick={handlelogin}>login</button>
     </div>
-  )
+  );
 };
 
 export default Login;
