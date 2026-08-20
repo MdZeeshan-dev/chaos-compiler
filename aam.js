bb224baf70a4f5033099442d41e17010458c430e
Md Zeeshan <zeeshan.codev@gmail.com>
2026-08-20 20:54:37 +0530

    h

---
+++ aam.js
@@ -0,0 +1,42 @@
+
+// const string = "Hello";
+// const number = 100;
+// const boolean = true;
+// const empty = null;
+// let notDefined;
+// const bigNumber = 12345678901234567890n;
+// const id = Symbol("id");
+
+// console.log(typeof string);
+// console.log(typeof number);
+// console.log(typeof boolean);
+// console.log(typeof empty);
+// console.log(typeof notDefined);
+// console.log(typeof bigNumber);
+// console.log(typeof id);
+
+
+
+// const user = {
+//   name: "Zeeshan",
+//   age: 21
+// };
+
+// const numbers = [1, 2, 3];
+
+// console.log(typeof user);
+// console.log(typeof numbers);
+
+
+// const value = "100";
+
+// console.log(Number(value));
+// console.log(String(100));
+// console.log(Boolean(1));
+
+// console.log(parseInt("100px"));
+// console.log(parseFloat("10.5px"));
+
+console.log(5 == "5");
+console.log(5 === "5");
+
---
+++ tempCodeRunnerFile.js
@@ -0,0 +1,6 @@
+
+// const string = "Hello";
+// const number = 100;
+// const boolean = true;
+// const empty = null;
+// let notDefined;
