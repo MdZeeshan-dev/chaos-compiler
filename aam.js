8b71afe61b5d7065580da206d59576cd23c9bd8d
Md Zeeshan <zeeshan.codev@gmail.com>
2026-08-21 11:43:52 +0530

    h

--- aam.js
+++ aam.js
@@ -69,10 +69,16 @@
 //   console.log(key, person[key]);
 // };
 
-for (let i = 0; i < 10; i++) {
-  if (i === 5)
-  {
-    break;
+// for (let i = 0; i < 10; i++) {
+//   if (i === 5) {
+//     break;
+//   }
+//   console.log(i);
+// }
+
+for (let i = 0; i < 4; i++) {
+  if (i === 2) {
+    continue;
   }
   console.log(i);
 }
